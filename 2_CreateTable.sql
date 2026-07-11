c625abcec0b407ecddd13b1d82e75015348a12c7
oyeitsyourvish <chinchanevishal03@gmail.com>
2026-07-11 06:59:07 +0530

    Added alter Drop query and Complete DDL and DML

--- 2_CreateTable.sql
+++ 2_CreateTable.sql
@@ -1,11 +1,15 @@
 
+-- to see All databases in Your Schemaa
+SELECT name
+FROM sys.databases;
+
 --Creating Database
 CREATE DATABASE SQLTUTORIAL;
 
 --Using that database
 USE SQLTUTORIAL;
 
---Creating Table inside SQLTUTORIAL db
+--Creating Table inside SQLTUTORIAL db (Its Come from DDL)
 CREATE TABLE Employee
 (
     Id INT PRIMARY KEY IDENTITY(100,1),  --EID is Variable, INT is datatype, PrimaryKey is Constrint and Identity(100,1) mean id start with 100 and update by 1.
@@ -21,7 +25,7 @@ CREATE TABLE Employee
     Job NVARCHAR(MAX)
 );
 
--- Inserting Data inside the Employee Table which is present in SQLTUTORIAL db
+-- Inserting Data inside the Employee Table which is present in SQLTUTORIAL db (Its come from DML)
 INSERT INTO Employee (Name, Email, Age, Salary, Gender, IsActive, DateOfBirth, LoginTime, CreatedDate, Job)
 Values
 ('vishal','vishalll@gmail.com',26,34000,'M',1,'1999-11-03','09:30:00',GetDate(),'dotnet developer'),
@@ -38,21 +42,30 @@ PRINT('DATA INSERTED SUCCESSFULLY')
 USE SQLTUTORIAL
 SELECT * FROM Employee
 
---Updating Column inside table in the basis of ID.
+--Updating Column inside table in the basis of ID.(Its Come from DML)
 UPDATE Employee SET DateOfBirth = '1999-10-03' WHERE Id = 108;
 PRINT ('Updated')
 
--- Delete Column inside table in the basis of ID
+-- Delete Column inside table in the basis of ID (Its Come From DML)
 DELETE from Employee where Id = 118;
 PRINT ('Deleted Successfully')
 
--- Delete All The Data From the Table
+-- Delete All The Data From the Table (Its Come From DML)
 DELETE from Employee ;
 PRINT('All Data Deleted')
 
--- Truncate Table table will delete all the row and reset the id from start again.
+-- Truncate Table tablename will delete all the row and reset the id from start again. (Its Come From DDL)
 TRUNCATE TABLE Employee ; 
 PRINT ('Truncated Successfully')
 
+-- Drop table tablename will drop the structure or table as well as data completely. (Its Come from DDL)
+DROP TABLE Xyz;
 
 
+-- Modify an existing object or add coloumn (Its come from DDL)
+ALTER TABLE Employee
+ADD City VARCHAR(100);
+
+
+-- DDL :- Create, Truncate, Drop, Alter
+-- DML :- Instert, Update, Delete, 
