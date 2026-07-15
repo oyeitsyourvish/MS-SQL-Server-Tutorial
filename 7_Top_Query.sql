8b7f85772c9ceab025c700998ed12ec28f4f0962
oyeitsyourvish <chinchanevishal03@gmail.com>
2026-07-15 05:39:16 +0530

    Created Emp table and Run TOP query

---
+++ 7_Top_Query.sql
@@ -0,0 +1,11 @@
+Use SQLTUTORIAL
+
+-- To Select Top 5 Rows use Top Query
+SELECT TOP 5 *
+FROM emp 
+ORDER BY id ASC
+
+-- To Select BOTTOM 5 Rows use Top Query
+SELECT TOP 5 *
+FROM emp 
+ORDER BY id DESC
